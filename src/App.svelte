9b154580b3c70cbc4b230fa0ef22c5bd1334a712
tatiana talavera <iced.tati@gmail.com>
2023-07-06 16:56:16 -0400

    making sure server works

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,20 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import axios from 'axios'
+  import PollList from './lib/PollList.svelte'
+  let promise = axios.get("http://127.0.0.1:3333/polls").then(({data} )=> data)
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  {#await promise}
+  <p>waiting for server...</p>
+  {:then polls}
+    <PollList {polls} />
+  {:catch error}
+  <p>error</p>
+  {/await}
+  <button>
+    This goes to CreatePoll
+  </button>
 </main>
 
 <style>
