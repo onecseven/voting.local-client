50d400e0885d1f78fa33c057548066afdeba1dcc
tatiana talavera <iced.tati@gmail.com>
2023-07-09 15:41:41 -0400

    moved

--- src/lib/Poll.svelte
+++ src/lib/Poll.svelte
@@ -1,52 +1,22 @@
 
 <script lang="ts">
-  //DISPLAYS THE SPECIFIED POLL
-  //DONE display the poll
-  //TODO make the poll draggable and stuff - "svelte-dnd-action"
-  //TODO name field require for posting rankings
-  //TODO make a submit button at the end that sends your ranking
-  //TODO make an add option thing
+  import { Link, navigate } from "svelte-routing"
   import { selected } from "../store";
-  import AddRanking from "./poll/AddRanking.svelte"
+  import SortableOptions from "./poll/SortableOptions.svelte"
 </script>
 
 {#if $selected}
 <h2><code>{$selected.name}</code></h2>
-<input placeholder="name input" type="text">
-<ul>
-  {#each $selected.options as option}
-  <button>
-    {option}
-  </button>
-  <br>
-  {/each}
-</ul>
-<AddRanking/>
-<button>
-  submit ranking
-</button>
+<SortableOptions />
 {:else}
-<p>nothing here</p>
+  <p> Nothing here...</p>
+  <Link to="/">
+    <button>
+      Reload
+    </button>
+  </Link>
 {/if}
 
 
 
-<style>
-    .callout {
-    background-color: rgba(63, 62, 62, 0.365);
-    border: 1px solid rgba(211, 211, 211, 0.386);
-    border: 0;
-    box-shadow: none;
-    margin: 0;
-    margin-left: 0px;
-    margin-left: 8px;
-    padding:0;
-    width: auto;
-    max-width: 20vw;
-    float: left;
-    min-width: 210px;
-  }
-  ul{
 
-  }
-</style>
