063b07d858b287c531f7639b3fb061c54b23b37a
tatiana talavera <iced.tati@gmail.com>
2023-07-09 16:34:38 -0400

    connected to rankings

--- src/lib/poll/SortableOptions.svelte
+++ src/lib/poll/SortableOptions.svelte
@@ -3,20 +3,28 @@
   import { getUniqueID } from "./getUniqueID"
   import { selected } from "../../store"
   import AddRanking from "./AddRanking.svelte"
+  import axios from "axios"
+  import { navigate } from "svelte-routing"
   const flipDurationMs = 200
-  let name = ""
+  let user = ""
 
   $: deco_items = $selected.options.map((option) => ({
     option,
     id: getUniqueID(),
   }))
+  
+  const submit = () => axios
+                      .post("http://127.0.0.1:3333/ranking", ({user, poll: $selected.name, ranking: deco_items.map(({option}) => option)}))
+                      .then(({data}) => selected.set(data) )
+                      .then(() => navigate("/rankings"))
+                      .catch(e => console.error('submit error'))
 
   function handleSort(e) {
     deco_items = e.detail.items
   }
 </script>
 
-<input bind:value={name} placeholder="What's your name?" type="text" />
+<input bind:value={user} placeholder="What's your name?" type="text" />
 <main>
   <ul
     class="callout"
@@ -26,11 +34,11 @@
   >
     {#each deco_items as item (item.id)}
       <button class="item">
-        ‖ {item.option}
+        ▸ {item.option}
       </button>
     {/each}
     <AddRanking />
-    <button> submit ranking </button>
+    <button disabled={!(user.length >= 2 && user.replace(/\s+/g, ""))}  on:click={submit} > submit ranking </button>
   </ul>
 </main>
 
@@ -47,10 +55,15 @@
     height:40px;
   }
 
+  button:hover {
+    border-bottom: 2px solid #d9d8dc;
+  }
   .item {
+    text-align: left;
     width: 100%;
     height: 40px;
     filter: hue-rotate(115deg);
+    font-size: larger;
   }
 
   .callout {
