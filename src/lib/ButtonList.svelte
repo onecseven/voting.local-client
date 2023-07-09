75e25b8f0a30979639bc13c0d94b05ea98cdc8d4
tatiana talavera <iced.tati@gmail.com>
2023-07-09 15:41:01 -0400

    ez component

---
+++ src/lib/ButtonList.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+export let items: string[]
+</script>
+
+<main>
+  <ul
+    class="callout"
+  >
+    {#each items as item}
+      <button class="item">
+        {item}
+      </button>
+    {/each}
+  </ul>
+</main>
+
+<style>
+  button {
+    width: 100%;
+    height:40px;
+  }
+
+  ul {
+    margin: 0;
+  }
+  ul > * {
+    margin: 2% 0 2% 0;
+  }
+
+  .item {
+    width: 100%;
+    height: 40px;
+    filter: hue-rotate(115deg);
+  }
+
+  .callout {
+    background-color: rgba(63, 62, 62, 0.365);
+    border: 1px solid rgba(211, 211, 211, 0.386);
+    border: 0;
+    box-shadow: none;
+    margin: 0;
+    margin-left: 0px;
+    padding: 0;
+    width: auto;
+    max-width: 20vw;
+    float: left;
+    min-width: 210px;
+  }
+</style>
